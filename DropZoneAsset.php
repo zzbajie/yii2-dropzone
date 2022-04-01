c6009af67a94468411e72a1f3181854e4ff98cbe
zzbajie <zzbajie@gmail.com>
2022-04-01 23:52:46 +0800

    v1.0.0

--- DropZoneAsset.php
+++ DropZoneAsset.php
@@ -6,18 +6,21 @@ use yii\web\AssetBundle;
 
 class DropZoneAsset extends AssetBundle
 {
-    public $css = [
-        'min/dropzone.min.css',
-    ];
-    public $js = [
-        'min/dropzone.min.js',
-    ];
-    public $depends = [
-        'yii\web\JqueryAsset',
-    ];
 
     public function init()
     {
         $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '/dist/';
     }
+
+    public $css = [
+        'min/dropzone.min.css',
+    ];
+
+    public $js = [
+        'min/dropzone.min.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
